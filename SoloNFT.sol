915c8dacadda303b03bf839f6a6ab98220790931
Tadej Fius <tadej.fius@gmail.com>
2021-07-27 19:18:42 +0200

    removed Counter

--- SoloNFT.sol
+++ SoloNFT.sol
@@ -86,33 +86,6 @@ library Strings {
         return string(buffer);
     }
 }
-library Counters {
-    struct Counter {
-        // This variable should never be directly accessed by users of the library: interactions must be restricted to
-        // the library's function. As of Solidity v0.5.2, this cannot be enforced, though there is a proposal to add
-        // this feature: see https://github.com/ethereum/solidity/issues/4637
-        uint256 _value; // default: 0
-    }
-
-    function current(Counter storage counter) internal view returns (uint256) {
-        return counter._value;
-    }
-
-    function increment(Counter storage counter) internal {
-        unchecked {
-            counter._value += 1;
-        }
-    }
-
-    function decrement(Counter storage counter) internal {
-        uint256 value = counter._value;
-        require(value > 0, "Counter: decrement overflow");
-        unchecked {
-            counter._value = value - 1;
-        }
-    }
-}
-
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {
         return msg.sender;
@@ -259,7 +232,7 @@ contract SoloNFT is Context, ERC165, IERC721, IERC721Metadata {
      * @dev See {IERC721-ownerOf}.
      */
     function ownerOf(uint256 tokenId) public view virtual override returns (address) {
-        require(tokenId == 1, "!token");
+        require(tokenId == 0, "!token");
         return owner;
     }
 
